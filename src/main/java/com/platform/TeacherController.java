ad8c5086b2cab1c650fb8d6ece52166bec4f3af8
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-09-12 08:22:17 -0400

    Updated new TeacherController.java

--- src/main/java/com/platform/TeacherController.java
+++ src/main/java/com/platform/TeacherController.java
@@ -1,4 +1,56 @@
 package com.platform;
+import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 public class TeacherController {
+    private final TeacherRepository repository;
+    TeacherController(TeacherRepository repository) {
+        this.repository = repository;
+    }
+
+    // Aggregate root
+
+    @GetMapping("/teacher")
+    List<Teacher> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/teacher")
+    Teacher newTeacher(@RequestBody Teacher newTeacher) {
+        return repository.save(newTeacher);
+    }
+
+    // Single item
+
+    @GetMapping("/teacher/{id}")
+    Teacher one(@PathVariable Long id) {
+
+        return repository.findById(id)
+                .orElseThrow(() -> new TeacherNotFoundException(id));
+    }
+    @PutMapping("/teacher/{id}")
+    Teacher replaceTeacher(@RequestBody Teacher newTeacher, @PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(employee -> {
+                    employee.setName(newTeacher.getName());
+                    employee.setCourse(newTeacher.getCourse());
+                    return repository.save(employee);
+                })
+                .orElseGet(() -> {
+                    newTeacher.setId(id);
+                    return repository.save(newTeacher);
+                });
+    }
+    @DeleteMapping("/teacher/{id}")
+    void deleteTeacher(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
 }
