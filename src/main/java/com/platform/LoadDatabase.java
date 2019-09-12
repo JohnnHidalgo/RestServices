f5482d3d21d7405baf86b58a1595d3e2022c5d19
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-09-11 22:51:38 -0400

    created LoadDatabase.java

---
+++ src/main/java/com/platform/LoadDatabase.java
@@ -0,0 +1,20 @@
+package platform;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+  @Bean
+  CommandLineRunner initDatabase(TeacherRepository repository) {
+    return args -> {
+      log.info("Preloading " + repository.save(new Teacher("Bilbo Baggins", "Matemática")));
+      log.info("Preloading " + repository.save(new Teacher("Frodo Baggins", "Física")));
+    };
+  }
+}
