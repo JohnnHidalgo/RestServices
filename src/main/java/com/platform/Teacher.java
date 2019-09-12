ca95445813d6d0c11ed53e08768e1d8811cc65a8
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-09-11 22:38:29 -0400

    update Teacher.java

---
+++ src/main/java/com/platform/Teacher.java
@@ -0,0 +1,23 @@
+package platform;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+class Teacher {
+
+  private @Id @GeneratedValue Long id;
+  private String name;
+  private String course;
+
+  Teacher() {}
+
+  Teacher(String name, String course) {
+    this.name = name;
+    this.course = course;
+  }
+}
