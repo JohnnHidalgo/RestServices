ac550b2f4221c9e2df1c1a99afb2d9a57ccb4536
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-09-12 16:39:42 -0400

    Update OrderController with methods

--- src/main/java/com/platform/OrderController.java
+++ src/main/java/com/platform/OrderController.java
@@ -1,4 +1,43 @@
 package com.platform;
+@RestController
+class OrderController {
 
-public class OrderController {
+    private final OrderRepository orderRepository;
+    private final OrderResourceAssembler assembler;
+
+    OrderController(OrderRepository orderRepository,
+                    OrderResourceAssembler assembler) {
+
+        this.orderRepository = orderRepository;
+        this.assembler = assembler;
+    }
+
+    @GetMapping("/orders")
+    Resources<Resource<Order>> all() {
+
+        List<Resource<Order>> orders = orderRepository.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+
+        return new Resources<>(orders,
+                linkTo(methodOn(OrderController.class).all()).withSelfRel());
+    }
+
+    @GetMapping("/orders/{id}")
+    Resource<Order> one(@PathVariable Long id) {
+        return assembler.toResource(
+                orderRepository.findById(id)
+                        .orElseThrow(() -> new OrderNotFoundException(id)));
+    }
+
+    @PostMapping("/orders")
+    ResponseEntity<Resource<Order>> newOrder(@RequestBody Order order) {
+
+        order.setStatus(Status.IN_PROGRESS);
+        Order newOrder = orderRepository.save(order);
+
+        return ResponseEntity
+                .created(linkTo(methodOn(OrderController.class).one(newOrder.getId())).toUri())
+                .body(assembler.toResource(newOrder));
+    }
 }
